e9ce0dd6286d3f5dccc65c9701bb5d9cd4ed256e
spontoreau <pontoreau.sylvain@gmail.com>
2018-08-15 11:18:32 +0200

    Add turns

--- guess_the_number/src/main.rs
+++ guess_the_number/src/main.rs
@@ -8,18 +8,25 @@ fn main() {
     println!("Welcome to guess the number! \nRules: 5 turns to retrieve a number between 1 and 100");
 
     let secret = rand::thread_rng().gen_range(1, 101);
+    let mut turn = 1;
 
-    println!("Please enter your guess:");
-    let mut guess = String::new();
+    while turn <= 5 {
+        println!("Turn {} - Please enter your guess:", turn);
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Read line error");
+        io::stdin().read_line(&mut guess).expect("Read line error");
 
-    let guess: u32 = guess.trim().parse().expect("Not a number");
-    println!("Your guess {}", guess);
+        let guess: u32 = guess.trim().parse().expect("Not a number");
+        println!("Your guess {}", guess);
 
-    match secret.cmp(&guess) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("Great job's, you win!")
+        match secret.cmp(&guess) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("Great job's, you win!")
+        }
+
+        turn = turn + 1;
     }
+
+    println!("You loose, game over!");
 }
