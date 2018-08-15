a6b3fa8226b8cad03c4702f7886808350f7c6f0b
spontoreau <pontoreau.sylvain@gmail.com>
2018-08-15 10:51:28 +0200

    Enter the guess number

--- guess_the_number/src/main.rs
+++ guess_the_number/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to guess the number! \nRules: 5 turns to retrieve a number between 1 and 100");
+
+    println!("Please enter your guess:");
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Read line error");
+
+    println!("Your guess {}", guess);
 }
