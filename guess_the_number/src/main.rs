476cb090aacee30d273d1d95f240915b15ee4464
spontoreau <pontoreau.sylvain@gmail.com>
2018-08-15 11:11:13 +0200

    Compare guess & secret

--- guess_the_number/src/main.rs
+++ guess_the_number/src/main.rs
@@ -2,6 +2,7 @@ extern crate rand;
 
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Welcome to guess the number! \nRules: 5 turns to retrieve a number between 1 and 100");
@@ -14,6 +15,11 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("Read line error");
 
     let guess: u32 = guess.trim().parse().expect("Not a number");
-
     println!("Your guess {}", guess);
+
+    match secret.cmp(&guess) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("Great job's, you win!")
+    }
 }
