b15b78010cbb2b22990973ca8f6d03e5bc55dd87
spontoreau <pontoreau.sylvain@gmail.com>
2018-08-22 22:17:02 +0200

    Update fibonacci function to use pattern matching instead of multiple conditions

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -13,13 +13,9 @@ fn main() {
 }
 
 fn fibonacci(n: i32) -> i32 {
-    if n == 0 {
-        return 0;
-    } 
-    else if n == 1 {
-        return 1;
-    }
-    else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
+    return match n {
+        0 => 0,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2)
+    };
 }
